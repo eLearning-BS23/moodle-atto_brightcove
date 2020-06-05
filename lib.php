1d9a1e724344bcba636ec0505d65753109b1f637
sahedbs23 <ssawon.bs23@gmail.com>
2020-06-05 11:15:24 +0600

    lib.php file  modified

--- lib.php
+++ lib.php
@@ -15,33 +15,49 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Atto text editor manage files plugin lib.
+ * Atto text editor import Microsoft Word files.
  *
- * @package    atto_managefiles
- * @copyright  2014 Frédéric Massart
+ * @package    atto_brightcove
+ * @copyright  2015 Eoin Campbell
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
+
+
 /**
- * Initialise the strings required for JS.
+ * Initialise the strings required for js
  *
  * @return void
  */
-function atto_managefiles_strings_for_js() {
+function atto_brightcove_strings_for_js() {
     global $PAGE;
-    $PAGE->requires->strings_for_js(array('managefiles'), 'atto_managefiles');
+
+    $strings = array(
+        'uploading',
+        'transformationfailed',
+        'fileuploadfailed',
+        'fileconversionfailed',
+        'pluginname'
+    );
+
+    $PAGE->requires->strings_for_js($strings, 'atto_brightcove');
 }
+
 
 /**
  * Sends the parameters to JS module.
  *
+ * @param string $elementid - unused
+ * @param stdClass $options the options for the editor, including the context
+ * @param stdClass $fpoptions - unused
  * @return array
  */
-function atto_managefiles_params_for_js($elementid, $options, $fpoptions) {
-    global $CFG, $USER;
-    require_once($CFG->dirroot . '/repository/lib.php');  // Load constants.
+function atto_brightcove_params_for_js($elementid, $options, $fpoptions) {
+
+    $brightcove_account = get_config('atto_brightcove', 'account');
+    $brightcove_player = get_config('atto_brightcove', 'player');
 
     // Disabled if:
     // - Not logged in or guest.
@@ -51,12 +67,12 @@ function atto_managefiles_params_for_js($elementid, $options, $fpoptions) {
             (!isset($options['maxfiles']) || $options['maxfiles'] == 0) ||
             (isset($options['return_types']) && !($options['return_types'] & ~FILE_EXTERNAL));
 
-    $params = array('disabled' => $disabled, 'area' => array(), 'usercontext' => null);
+    $params = array('disabled' => $disabled, 'area' => array(), 'usercontext' => null,
+        'brightcove_account' => $brightcove_account, 'brightcove_player' =>$brightcove_player);
 
     if (!$disabled) {
         $params['usercontext'] = context_user::instance($USER->id)->id;
-        foreach (array('itemid', 'context', 'areamaxbytes', 'maxbytes', 'subdirs', 'return_types',
-                       'removeorphaneddrafts') as $key) {
+        foreach (array('itemid', 'context', 'areamaxbytes', 'maxbytes', 'subdirs', 'return_types') as $key) {
             if (isset($options[$key])) {
                 if ($key === 'context' && is_object($options[$key])) {
                     // Just context id is enough.
@@ -70,3 +86,6 @@ function atto_managefiles_params_for_js($elementid, $options, $fpoptions) {
 
     return $params;
 }
+
+
+
