67d3342aec22d4bde2355671a8f80a49f91065bf
Sahed moral <55475814+sahedbs23@users.noreply.github.com>
2020-07-30 12:59:02 +0600

    Update lib.php

--- lib.php
+++ lib.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- *@package     atto_brightcove
+ * @package     atto_brightcove
  * @copyright   2020 Brain station 23 ltd. <https://brainstation-23.com/>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
