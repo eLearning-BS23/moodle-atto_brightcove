aee6dd282f461fd6da196a0449a8a42e682260e3
Sahed moral <55475814+sahedbs23@users.noreply.github.com>
2020-07-30 12:50:18 +0600

    Update install.php

--- db/install.php
+++ db/install.php
@@ -15,31 +15,13 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Atto text editor import Microsoft Word files - installation.
+ * Atto brightcove extention - installation.
  *
- * @package    atto_brightcove
- * @copyright  2015 Eoin Campbell
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package     atto_brightcove
+ * @copyright   2020 Brain station 23 ltd. <https://brainstation-23.com/>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @license    see original copyright notice below
  */
-
-//
-// Copyright (c) 2011, Maths for More S.L. http://www.wiris.com
-// This file is part of WIRIS Plugin.
-//
-// WIRIS Plugin is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// any later version.
-//
-// WIRIS Plugin is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with WIRIS Plugin. If not, see <http://www.gnu.org/licenses/>.
-//
 
 defined('MOODLE_INTERNAL') || die();
 
