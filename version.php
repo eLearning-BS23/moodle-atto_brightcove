17116faf1146fc8d86a4d269c823ecf42d0831fd
Anowar Hossain <anowar.cst@gmail.com>
2020-09-09 18:30:35 +0600

    Update version.php

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020052000;
+$plugin->version   = 2020052001;
 $plugin->requires  = 2019111803; // Requires Moodle 3.8.3
 $plugin->component = 'atto_brightcove';  // Full name of the plugin (used for diagnostics).
 $plugin->release   = '1.0.1 (Build 2019112200)'; // Human readable version information.
